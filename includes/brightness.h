c7b451931e3c471c7e8e17744fd3a3052f890084
Sousini <j.asousa2004@gmail.com>
2023-04-17 11:19:16 +0100

    ficheiros brightness

---
+++ includes/brightness.h
@@ -0,0 +1,10 @@
+#ifndef __BRIGHTNESS_H__
+#define __BRIGHTNESS_H__
+
+
+
+
+
+
+
+#endif
---
+++ src/brightness.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ncurses.h>
+#include "main.h"
+
